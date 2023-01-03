a682f1270aa16a7bdc2a81781f6763b4a6051124
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2023-01-03 17:23:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RecipeSearch from './components/RecipeSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeSearch />
+    </div>
+  );
+}
+
+export default App;
